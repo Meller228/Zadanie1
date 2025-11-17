10df032e02ba93a028ebf8309f8d183202b74928
Meller228 <sukinmisa465@gmail.com>
2025-11-17 14:40:10 +0300

    Практическая 1

---
+++ dask1.py
@@ -0,0 +1,56 @@
+## #Задание 1
+# first_num = 9
+# second_num = 7.8
+# my_str = "start"
+# print(first_num, second_num, my_str)
+# first_num = 5.2
+# print(first_num, type(first_num))
+# third_num = second_num + first_num
+# print(third_num, type(third_num))
+# first_num = 5
+# second_num = 10.2
+# print(second_num, first_num)
+# second_num = int(second_num)
+# print(second_num, first_num)
+# my_str = my_str + "&stop"
+# print(my_str)
+# print(my_str * 5)
+#Задание 2
+# path = "С:\\Users\\MainAdmin\\Desktop\\programs"
+# print(path)
+# path = path + "\\game.exe"
+# print(path)
+# path = "С:\\Users\\MainAdmin\\Desktop\\files"
+# path = path + "\\picture.png"
+# print(path)
+# path = "C:\\Games\\city simulator"
+# print(path)
+# print("Error path:", path * 2)
+#Задание 3
+# num_1 = 7
+# num_2 = 10
+# num_3 = 4
+# summ = num_1 + num_2 + num_3
+# print("Сумма всех нецелых чисел:",summ)
+# num_1 = 7.9
+# num_2 = 21.3
+# num_3 = float(num_3)
+# summ = num_1 + num_2 + num_3
+# print("Сумма всех нецелых чисел:",summ)
+# num1 = 130
+# num2 = 4
+# num3 = 2
+# multiplying = num1 * num2 * num3
+# print("Умножение всех чисел:", multiplying)
+# division = (num1 / num2 ) / num3
+# print("Деление:",division)
+# num1 = 2
+# num2 = 3
+# num3 = 4
+# degree = (num1 ** num2 ** num3)
+# print("Числа в степени:",degree)
+# num1 = 2
+# num2 = 8
+# num3 = 5
+# math = ((43 + num1) + (num2 + 67) - (num3 * 2)) // 2
+# print(math)
